05bb34ce7eb27830fbcc086b601bc98c397e450a
Efty34 <efty17.hasan@gmail.com>
2024-10-16 22:25:17 +0600

    Firebase connection established

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:txt/auth/login_or_register.dart';
+import 'package:txt/firebase_options.dart';
 import 'package:txt/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,3 +22,21 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/*
+  ---------------
+  npm install -g firebase-tools
+  ---------------
+  firebase login
+  ---------------
+  flutter pub global activate flutterfire_cli
+  ---------------
+  flutterfire configure
+  ---------------
+  flutter pub add firebase_core
+  ---------------
+  flutter pub add firebase_auth
+  ---------------
+
+
+*/
